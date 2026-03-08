c6639649c16bdec682ba2b34d7c3cda00f7fde97
Jigoogle <ajaytripathi821@gmail.com>
2026-03-08 08:19:18 +0530

    first commit

---
+++ FirstFile.cs
@@ -0,0 +1,32 @@
+//String is Mutable it is changes in its most recent state.
+//int
+
+//Bool
+// int a  = "Ankush";
+// Console.WriteLine("Value of str "+a);
+
+//Keywords
+
+//Keywords are the predefined set of reserved words that have special meaning in a program 
+ 
+
+class Program
+{
+    public static void Main(String[]args)
+    {
+        string name;
+        Console.Write("enter You Name: ");
+        name=Console.ReadLine();
+
+        Console.Write(name);
+    }
+}
+
+
+
+
+
+
+
+
+
