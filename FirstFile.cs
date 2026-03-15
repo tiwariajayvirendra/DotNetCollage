aaa515053c9def434a6b7bafc1802fb4ed5cf25d
Jigoogle <ajaytripathi821@gmail.com>
2026-03-15 12:48:44 +0530

    C# For Collage Exams

--- FirstFile.cs
+++ FirstFile.cs
@@ -9,22 +9,66 @@
 
 //Keywords are the predefined set of reserved words that have special meaning in a program 
  
+////////////////////////////////////////////////////////////
+// class Program
+// {
+//     public static void Main(String[]args)
+//     {
+//         string name;
+//         Console.Write("enter You Name: ");
+//         name=Console.ReadLine();
 
+//         Console.Write(name);
+//     }
+// }
+
+///////////////////////////////////////////////////////////
+using System;
+
+// class FirstFile
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Enter a number:");
+//         int number = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("You entered: " + number);
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////
+
+
+//String valeu --- Read Line()
+// Interger Value  -- ToInt32()
 class Program
 {
-    public static void Main(String[]args)
+    public static void Main(String[] args)
     {
-        string name;
-        Console.Write("enter You Name: ");
-        name=Console.ReadLine();
+        // int a= 10, b=20;
+        // Console.Write("Enter Any Two Number:");
+        // a = Convert.ToInt32
+    // data Types 
+    // bool str = true;
+         
+    // Console.WriteLine("Enter any vlue "+str);
 
-        Console.Write(name);
+// Keywords 77  but now in 2026 79 diffrent contextext like controlflow if,else,switch,for,while,do , break,continue,, Type Keywords int,float, double,string ,bool, decimal, object, A
+// Access Modifiers;public ,private , protected, internal , Others class struct , interface , namespace, using , retrun , new ,try, catch, finally
+// contextual keywords(25)    var, dynamic,async, await ,yield , partial,
+    //int num
+    string name;
+    Console.Write("Enter any number:");
+    name= Console.ReadLine();
+    int num = Convert.ToInt32(name);
+
+    Console.Write("My Value "+num);
+
+
+
+
+
     }
 }
-
-
-
-
 
 
 
--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-namespace First
-{
-    class Program
-    {
-    static void  Main(string[]args)
-        {
-            //First Program
-            //int a = 10;
-            // a = 24;
-            double num= 45.5;
-            Console.WriteLine(num);
-        }
-    }
-}
+﻿// using System;
+// namespace First
+// {
+//     class Program
+//     {
+//     static void  Main(string[]args)
+//         {
+//             //First Program
+//             //int a = 10;
+//             // a = 24;
+//             double num= 45.5;
+//             Console.WriteLine(num);
+//         }
+//     }
+// }
